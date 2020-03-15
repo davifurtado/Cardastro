ba9172d38f4a0ffedd0656299463bd48fa88f22f
davifurtado <44845457+davifurtado@users.noreply.github.com>
2020-03-14 22:02:46 -0300

    created empty CarDetails file, created CarList pure function

---
+++ src/components/CarList.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { CarContext } from '../contexts/CarContext';
+import { CarDetails } from '../components/CarDetails';
+
+const CarList = () => {
+    const { cars } = useContext(CarContext);
+    return cars.length ? (
+        <div className="car-list">
+            <ul>
+                { cars.map(car => {
+                    return ( <CarDetails car={car} key={car.id} />)
+                })}
+            </ul>
+        </div>
+    ) : (
+        <div className="empty">
+            Nenhum veículo encontrado
+        </div>
+    )
+}
+
+export default CarList
