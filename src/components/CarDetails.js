4db710b1a70dd1f688eff4b5053e9be5380085dd
davifurtado <44845457+davifurtado@users.noreply.github.com>
2020-03-14 23:12:44 -0300

    CarContext corrected the dummy data to have an ID, added removeCar in CarDetails to remove on X click

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -10,6 +10,7 @@ const CarDetails = ({ car }) => {
             <div className="modelo">{ car.modelo }</div>
             <div className="ano">{ car.ano }</div>
             <div className="quilometragem">{ car.quilometragem }</div>
+            <div onClick={() => removeCar(car.id)}>X</div>
         </li>
     );
 }
