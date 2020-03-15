6af1e2a89846e37f7577620b72d7ef4bd9c66550
davifurtado <44845457+davifurtado@users.noreply.github.com>
2020-03-14 22:09:15 -0300

    corrected the import in CarList, created pure function for CarDetails to show the details of the cars

---
+++ src/components/CarDetails.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { CarContext } from '../contexts/CarContext';
+
+const CarDetails = ({ car }) => {
+    const { removeCar } = useContext(CarContext)
+    return (
+        <li>
+            <div className="placa">{ car.placa }</div>
+            <div className="marca">{ car.marca }</div>
+            <div className="modelo">{ car.modelo }</div>
+            <div className="ano">{ car.ano }</div>
+            <div className="quilometragem">{ car.quilometragem }</div>
+        </li>
+    );
+}
+
+export default CarDetails
