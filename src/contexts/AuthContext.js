d7f8a8f948aa6db6565456537ef3e9d85e6f8397
davifurtado <44845457+davifurtado@users.noreply.github.com>
2020-03-15 17:18:12 -0300

    added localStorage for AuthContext and CarContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,11 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 // O sistema de login pode ser feito através do context para englobar a aplicação inteira sem precisar de redux. 
 const AuthContextProvider = (props) => {
-    const [user, setUser] = useState([
-    ]);
+    const localUser = localStorage.getItem('user')
+    const [user, setUser] = useState(
+        localUser ? JSON.parse(localUser) : []
+    );
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user))
+    }, [user])
     const addUser = (user, password) => {
         setUser([{ user, password, id: 1 }])
     }
--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -1,14 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import uuid from 'uuid/v1';
 
 export const CarContext = createContext();
 
 const CarContextProvider = (props) => {
-    const [cars, setCars] = useState([
-        { placa: '123ABC', marca: 'Ford', modelo: 'Ka', ano: '2010', quilometragem: '2000', id: 1 },
-        { placa: 'abc123', marca: 'Volks', modelo: 'Ka', ano: '2010', quilometragem: '2000', id: 2 },
-        { placa: '12ab3c', marca: 'Chevrolet', modelo: 'Celta', ano: '2010', quilometragem: '2000', id: 3 }
-    ]);
+    const localData = localStorage.getItem('cars')
+    const [cars, setCars] = useState(
+        localData ? JSON.parse(localData) : []
+    );
+    useEffect(() => {
+        localStorage.setItem('cars', JSON.stringify(cars))
+    }, [cars])
     const addCar = (placa, marca, modelo, ano, quilometragem) => {
         // a função uuid serve para criar um id único aleatório
         setCars([...cars, {placa, marca, modelo, ano, quilometragem, id: uuid() }])
