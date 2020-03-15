6ea2b6d11d7c14212d344cd98fadc4dbdea97e89
davifurtado <44845457+davifurtado@users.noreply.github.com>
2020-03-14 22:15:31 -0300

    added CarList in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CarContextProvider from './contexts/CarContext';
 import Navbar from './components/Navbar';
+import CarList from './components/CarList';
 
 function App() {
   return (
     <div className="App">
       <CarContextProvider>
         <Navbar />
+        <CarList />
       </CarContextProvider>
     </div>
   );
